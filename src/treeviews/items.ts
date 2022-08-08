67567191435f47971e13749c5c7cc62d5af2842f
Danila Fominykh <d.fominykh@ctrl2go.solutions>
2022-08-08 22:34:18 +0300

    added icon for hosts

--- src/treeviews/items.ts
+++ src/treeviews/items.ts
@@ -44,7 +44,7 @@ export class HostsItem extends ClickerItem {
     super.tooltip = `Hosts for gRPC calls for current file.`;
     super.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
     super.contextValue = `hosts`;
-    const icon = `field.svg`;
+    const icon = `hosts.svg`;
     super.contextValue = "hosts";
     super.iconPath = {
       light: path.join(__filename, "..", "..", "images", icon),
